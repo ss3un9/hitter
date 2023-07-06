23dae9222996cb32555aee931d460cf951b2028c
gerbon <feb_ye@naver.com>
2023-07-06 12:03:13 +0900

    완료

--- project/src/main/frontend/src/page/signup/SignupPage.js
+++ project/src/main/frontend/src/page/signup/SignupPage.js
@@ -229,20 +229,22 @@ const SignupPage = () => {
                     </script>
 
                     <form className='agree-list'>
-                        <ul className="join_box">
-                            <li className="checkBox-check02">
+                        <div className="join_box">
+                            <div className="checkBox-check02">
                                 <br></br><br></br>
-                                <li>이용약관 동의(필수)
+                                <div>이용약관 동의(필수)
                                     <input type="checkbox" name="chk" checked={isTermsChecked}
                                            onChange={handleTermsCheckboxChange}/>
-                                </li>
+                                </div>
                                 <br></br>
+                            </div>
+                            <div className='txtarea'>
                                 <textarea className='first' name="" id="">
                                     여러분을 환영합니다.
                                     히터(HITTER) 서비스 및 제품(이하 ‘서비스’)을 이용해 주셔서 감사합니다.
                                     본 약관은 다양한 히터(HITTER) 서비스의 이용과 관련하여 히터(HITTER) 서비스를 제공하는 히터(HITTER) 주식회사(이하 ‘히트시그널’)와 이를 이용하는 히터(HITTER) 서비스 회원(이하 ‘회원’) 또는 비회원과의 관계를 설명하며, 아울러 여러분의 히터(HITTER) 서비스 이용에 도움이 될 수 있는 유익한 정보를 포함하고 있습니다.
                                 </textarea>
-                            </li>
+                            </div>
                             <li className="checkBox-check03">
                                 <br></br><br></br>
                                 <li>개인정보 수집 및 이용에 대한 안내(필수)
@@ -287,7 +289,7 @@ const SignupPage = () => {
 이용자는 개인정보의 수집 및 이용 동의를 거부할 권리가 있습니다. 회원가입 시 수집하는 최소한의 개인정보, 즉, 필수 항목에 대한 수집 및 이용 동의를 거부하실 경우, 회원가입이 어려울 수 있습니다.
                                 </textarea>
                             </li>
-                        </ul>
+                        </div>
                     </form>
 
                     <div className="jumbotron">
